7da1f99998dd7ecb71c052f777ef1c94f2fc48e3
Anatoliy Smilik <cvbkbr@gmail.com>
2020-01-08 19:43:25 +0500

    lesson 16 detail record

--- src/store/category.js
+++ src/store/category.js
@@ -31,6 +31,17 @@ export default {
         commit('setError', e)
         throw e
       }
-    }
+    },
+    async fetchCategoryById ({ commit, dispatch }, id) {
+      try {
+        const uid = await dispatch('getUid')
+        const category = (await firebase.database().ref(`/users/${uid}/categories`).child(id).once('value')).val() || {}
+
+        return { ...category, id}
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
   }
 }
--- src/views/DetailRecord.vue
+++ src/views/DetailRecord.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <div>
+    <Loader v-if="loading" />
+    <p class="center" v-else-if="!record">Запись с id {{$route.params.id}} не найдена</p>
+    <div v-else>
       <div class="breadcrumb-wrap">
-        <a href="/history" class="breadcrumb">История</a>
-        <a class="breadcrumb">
-          Расход
+        <router-link to="/history" class="breadcrumb">История</router-link>
+        <a class="breadcrumb" @click.prevent>
+          {{record.type === 'income' ? 'Доход' : 'Расход'}}
         </a>
       </div>
       <div class="row">
         <div class="col s12 m6">
-          <div class="card red">
+          <div class="card" :class="record.type === 'income' ? 'green' : 'red'">
             <div class="card-content white-text">
-              <p>Описание:</p>
-              <p>Сумма:</p>
-              <p>Категория:</p>
+              <p>Описание: {{record.description}}</p>
+              <p>Сумма: {{record.amount}}</p>
+              <p>Категория: {{record.categoryName}}</p>
 
-              <small>12.12.12</small>
+              <small>{{record.date | date('datetime')}}</small>
             </div>
           </div>
         </div>
@@ -26,7 +28,22 @@
 
 <script>
   export default {
-    name: 'DetailRecord'
+    name: 'DetailRecord',
+    data: () => ({
+      record: null,
+      loading: true,
+    }),
+    async mounted() {
+      const id = this.$route.params.id
+      const record = await this.$store.dispatch('fetchRecordById', id)
+
+      if (Object.keys(record).length) {
+        const category = await this.$store.dispatch('fetchCategoryById', record.catId)
+        this.record = {...record, categoryName: category.title}
+      }
+
+      this.loading = false
+    }
   }
 </script>
 
